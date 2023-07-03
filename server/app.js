d2ff4c4e5044f460627029e944b6aeeb20e8e45f
useonguk <ysu0219@gmail.com>
2023-07-03 15:27:31 +0900

    express

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const router = require("./routers");
 const app = express();
 
-const dataBase = ["할1"];
-app.get("/", function (req, res) {
-  res.json(dataBase);
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", router);
 
 app.listen(4000);
---
+++ server/routers/todo.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+
+const dataBase = [{ id: 1, text: "할1" }];
+let currentid = 1;
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+router.get("/", function (req, res) {
+  res.json(dataBase);
+});
+
+router.post("/", (req, res) => {
+  const data = req.body;
+  console.log("data : ", data);
+  dataBase.push({ id: ++currentid, text: data.text });
+  return res.json("성공");
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  dataBase = dataBase.filter((data) => data.id !== Number(id));
+  return res.json("성공");
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const updataIndex = dataBase.findIndex((data) => data.id === Num(id));
+  dataBase[updataIndex].text = req.body.text;
+  return res.json("성공");
+});
+
+module.exports = router;
