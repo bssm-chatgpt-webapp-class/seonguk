75fe4906859663bb158d18823ee5dc056213c3d7
useonguk <ysu0219@gmail.com>
2023-07-04 16:07:58 +0900

    add sql

---
+++ server/table.sql
@@ -0,0 +1,14 @@
+create table todo (
+    id float primary key auto_increment,
+    todo text not null,
+    completed boolean not null default false,
+  user_id int not null
+); 
+
+INSERT INTO todo(todo, completed) VALUES('todo1', true);
+
+CREATE TABLE user (
+id INT PRIMARY KEY auto_increment,
+email varchar(255) not null,
+pw varchar(255) not null
+);
